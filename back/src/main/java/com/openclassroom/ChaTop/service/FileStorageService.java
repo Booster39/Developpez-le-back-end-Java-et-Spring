6cd5feaadc3beaef161a88ef2b743b12d6994389
Booster39 <abdiame10@yahoo.fr>
2024-06-24 10:22:44 +0200

    feat: picturesPath variable added to properties

--- back/src/main/java/com/openclassroom/ChaTop/service/FileStorageService.java
+++ back/src/main/java/com/openclassroom/ChaTop/service/FileStorageService.java
@@ -1,7 +1,7 @@
 package com.openclassroom.ChaTop.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.beans.factory.annotation.Value;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,37 +11,12 @@ import java.util.UUID;
 @Service
 public class FileStorageService {
   private final Path storageLocation;
-/*
-  @Value("${application.pictures.path}")
-  private String picturesPath;
 
-  @Value("${application.pictures.url}")
+  @Value("${oc.app.pictures.url}")
   private String picturesUrl;
 
-  @Override
-  public String savePicture(MultipartFile picture) {
-    File pictureFolder = new File(picturesPath);
-
-    // If picture folder does not exist, create it
-    if (!pictureFolder.exists() && pictureFolder.mkdirs() || pictureFolder.exists() && pictureFolder.isDirectory()) {
-      // That is the new name of the picture, we use UUID to avoid conflicts
-      String pictureFileName = UUID.randomUUID().toString();
-      File pictureFile = new File(pictureFolder, pictureFileName);
-
-      try {
-        // Saving the picture to the file and returning the URL
-        picture.transferTo(pictureFile);
-        return picturesUrl + "/" + pictureFileName;
-      } catch (Exception e) {
-        logger.error("Failed to save picture", e);
-      }
-    }
-    throw new ResponseEntityException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to save picture");
-  }
-*/
-
   public FileStorageService() {
-    this.storageLocation = Paths.get("ChaTopBack/src/main/resources/static/public/");
+    this.storageLocation = Paths.get("back/src/main/resources/static/public/");
     try {
       Files.createDirectories(this.storageLocation);
     } catch (IOException e) {
@@ -56,7 +31,7 @@ public class FileStorageService {
       // Copy the file to the target location
       Path targetLocation = this.storageLocation.resolve(fileName);
       Files.copy(file.getInputStream(), targetLocation);
-      return  fileName; // Return the path to the stored file
+      return picturesUrl+fileName; // Return the path to the stored file
     } catch (IOException e) {
       throw new RuntimeException("Could not store file " + fileName, e);
     }
